9aaef9dc1c2fa9f5f037450e206f335cb2a2c0ef
AR-CADE <armelcadetpetit@gmail.com>
2023-07-20 14:52:12 +0200

    Add a timeout for the video loader

--- lib/src/style/video_loading_style.dart
+++ lib/src/style/video_loading_style.dart
@@ -3,7 +3,12 @@ import 'package:petit_player/src/style/loader.dart';
 
 /// Video Loading Style
 class VideoLoadingStyle {
-  VideoLoadingStyle({this.loading = const Loader()});
+  VideoLoadingStyle(
+      {this.loading = const Loader(), this.timeout = const Duration()});
 
+  /// THe Loader Widget
   final Widget loading;
+
+  /// The minnimum of time this loader will be shown
+  final Duration timeout;
 }
