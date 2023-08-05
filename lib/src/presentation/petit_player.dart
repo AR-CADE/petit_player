28e65b0adf6a4db652472a5ec630b325423f9d7a
AR-CADE <armelcadetpetit@gmail.com>
2023-08-05 20:42:14 +0200

    Convert petit_player class to StatelessWidget

--- lib/src/presentation/petit_player.dart
+++ lib/src/presentation/petit_player.dart
@@ -6,7 +6,7 @@ import 'package:petit_player/src/core/style/video_loading_style.dart';
 import 'package:petit_player/src/core/utils/utils.dart';
 import 'package:petit_player/src/presentation/player/player.dart';
 
-class PetitPlayer extends StatefulWidget {
+class PetitPlayer extends StatelessWidget {
   const PetitPlayer({
     required this.uri,
     super.key,
@@ -45,23 +45,18 @@ class PetitPlayer extends StatefulWidget {
   final bool keepAspectRatio;
 
   @override
-  State<PetitPlayer> createState() => _PetitPlayerState();
-}
-
-class _PetitPlayerState extends State<PetitPlayer> {
-  @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => PlayerBloc(),
       child: PlayerView(
-        uri: widget.uri,
-        videoLoadingStyle: widget.videoLoadingStyle,
-        streamController: widget.streamController,
-        autoPlay: widget.autoPlay,
-        aspectRation: widget.aspectRation,
-        keepAspectRatio: widget.keepAspectRatio,
-        httpHeaders: widget.httpHeaders,
-        engine: widget.engine,
+        uri: uri,
+        videoLoadingStyle: videoLoadingStyle,
+        streamController: streamController,
+        autoPlay: autoPlay,
+        aspectRation: aspectRation,
+        keepAspectRatio: keepAspectRatio,
+        httpHeaders: httpHeaders,
+        engine: engine,
       ),
     );
   }
