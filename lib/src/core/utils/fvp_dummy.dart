1f3bbce0a725cb019bbd480bad08bb7ba352beba
AR-CADE <armelcadetpetit@gmail.com>
2023-08-17 11:27:24 +0200

    Add basic support for FVP

---
+++ lib/src/core/utils/fvp_dummy.dart
@@ -0,0 +1,21 @@
+/// a Simple and naive mdk dummy class,
+/// that help to compile with flutter web
+class MdkVideoPlayer {
+  static dynamic registerWith({dynamic options}) {}
+  static dynamic close() {}
+}
+
+// ignore: non_constant_identifier_names
+dynamic PlaybackState;
+// ignore: non_constant_identifier_names
+dynamic State;
+
+class Player {
+  dynamic dispose() {}
+  dynamic updateTexture() {}
+  dynamic state;
+  dynamic textureId;
+  dynamic media;
+  dynamic loop;
+  dynamic videoDecoders;
+}
---
+++ lib/src/presentation/player/widgets/fvp_texture_player.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class FvpTexturePlayer extends StatelessWidget {
+  const FvpTexturePlayer({
+    required this.textureId,
+    super.key,
+  });
+
+  final ValueNotifier<int?> textureId;
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<int?>(
+      valueListenable: textureId,
+      builder: (context, id, _) {
+        if (id == null || id == -1) {
+          return const SizedBox.expand();
+        }
+        return Texture(textureId: id);
+      },
+    );
+  }
+}
